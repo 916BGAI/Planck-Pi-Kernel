a8c089b7f689b6d73b2a68a9095c38aac52440b0
916BGAI <916772719@qq.com>
2023-04-05 10:53:19 +0800

    commit

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 47 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#47 Wed Aug 31 19:22:06 CST 2022"
+#define LINUX_COMPILE_BY "kjlsai"
+#define LINUX_COMPILE_HOST "Arch"
+#define LINUX_COMPILER "gcc version 7.5.0 (Linaro GCC 7.5-2019.12)"
